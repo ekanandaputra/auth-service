37ce6f46ef303b0d29d1f29f682ac2bad63771b3
ekanandaputra <ekananda.putra12@gmail.com>
2026-04-07 14:38:31 +0700

    apply gitignore

--- dist/config/redis.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.redisClient = void 0;
-exports.redisClient = null;
-// if (env.REDIS_URL) {
-//   redisClient = new Redis(env.REDIS_URL);
-//   redisClient.on('connect', () => console.log('Redis connected'));
-//   redisClient.on('error', (err) => console.error('Redis error', err));
-// }
