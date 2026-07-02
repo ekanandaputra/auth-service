7cf0cb26f6b46d9b6132a56dd1b38341519e328d
ekanandaputra <ekananda.putra12@gmail.com>
2026-07-02 07:43:48 +0700

    feat: update database schema to include user metadata, role keys, and unit management tables

--- prisma/migrations/20260702004334_init/migration.sql
+++ prisma/migrations/20260702004334_init/migration.sql
@@ -1,22 +1,29 @@
 -- CreateTable
 CREATE TABLE `users` (
     `id` VARCHAR(36) NOT NULL,
-    `email` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NULL,
+    `name` VARCHAR(191) NULL,
+    `nip` VARCHAR(191) NULL,
+    `type` ENUM('EMPLOYEE', 'LECTURER') NULL,
     `password` VARCHAR(191) NOT NULL,
+    `isActive` BOOLEAN NOT NULL DEFAULT true,
+    `deletedAt` DATETIME(3) NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
     UNIQUE INDEX `users_email_key`(`email`),
+    UNIQUE INDEX `users_nip_key`(`nip`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `roles` (
     `id` VARCHAR(36) NOT NULL,
+    `key` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NOT NULL,
     `description` VARCHAR(191) NULL,
 
-    UNIQUE INDEX `roles_name_key`(`name`),
+    UNIQUE INDEX `roles_key_key`(`key`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -56,6 +63,26 @@ CREATE TABLE `user_permissions` (
     PRIMARY KEY (`userId`, `permissionId`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+-- CreateTable
+CREATE TABLE `units` (
+    `id` VARCHAR(36) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `units_name_key`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `user_units` (
+    `userId` VARCHAR(36) NOT NULL,
+    `unitId` VARCHAR(36) NOT NULL,
+
+    PRIMARY KEY (`userId`, `unitId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 -- AddForeignKey
 ALTER TABLE `user_roles` ADD CONSTRAINT `user_roles_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
@@ -73,3 +100,9 @@ ALTER TABLE `user_permissions` ADD CONSTRAINT `user_permissions_userId_fkey` FOR
 
 -- AddForeignKey
 ALTER TABLE `user_permissions` ADD CONSTRAINT `user_permissions_permissionId_fkey` FOREIGN KEY (`permissionId`) REFERENCES `permissions`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `user_units` ADD CONSTRAINT `user_units_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `user_units` ADD CONSTRAINT `user_units_unitId_fkey` FOREIGN KEY (`unitId`) REFERENCES `units`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
